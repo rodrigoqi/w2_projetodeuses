7bc50792e1b90ae76c7783720a0190663e444189
Rodrigo Barreto <rodrigo.barreto@qi.edu.br>
2020-12-16 19:11:45 -0300

    Projeto de cadastro de deuses

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<!--Tags básicas do head-->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Deuses Nórdicos</title>
+	<!--Link dos nossos arquivos CSS e JS padrão-->
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="css/deuses.css">
+	<script src="js/scripts.js"></script>
+	<!--Link dos arquivos CSS e JS do Bootstrap-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+	<div class="container">
+		<br>
+		<div class="row text-center">
+			<div class="col-md-12">
+				<img class="ajustavel" src="img/header.png">
+			</div>
+		</div>
+		<br>
+		<div class="row" id="titulo">
+			<div class="col-md-12 text-center">
+				<h1>Deuses Nórdicos</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-4 text-center cardgod">
+				Deus 1
+			</div>
+			<div class="col-md-4 text-center cardgod">
+				Deus 2
+			</div>
+			<div class="col-md-4 text-center cardgod">
+				Deus 3
+			</div>
+			<div class="col-md-4 text-center cardgod">
+				Deus 4
+			</div>
+		</div>
+		
+
+	</div>
+</body>
+
+</html>
